75718f7d99977cbf277fe51980bf5b9d22489983
owbird <owbirdphorccuo@gmail.com>
2023-11-14 10:13:23 +0000

    Fix (page fetcher): Remove test url

--- utils/fetchPage.go
+++ utils/fetchPage.go
@@ -12,7 +12,7 @@ import (
 // href: The URL of the web page to fetch.
 func FetchPage(href string) string {
 
-	resp, err := soup.Get("https://webhook.site/6e360e38-144a-4c80-a62c-5df961c9773a")
+	resp, err := soup.Get(href)
 
 	if err != nil {
 		for {
