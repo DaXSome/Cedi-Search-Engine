db37b8729901dcca3284b12c34c69f168908bb00
owbird <owbirdphorccuo@gmail.com>
2023-11-17 04:40:40 +0000

    Chore: Optimize page fetching

--- utils/fetchPage.go
+++ utils/fetchPage.go
@@ -1,18 +1,24 @@
 package utils
 
 import (
+	"log"
+
 	"github.com/go-rod/rod"
 	"github.com/go-rod/rod/lib/proto"
 )
+
+var browser = rod.New().MustConnect().WithPanic(func(i interface{}) {
+	log.Println("[!] Headerless browser proberly lost context.")
+})
 
 // FetchPage fetches the content of a web page given its URL.
 //
 // href: The URL of the web page to fetch.
 func FetchPage(href string) string {
 
-	browser := rod.New().MustConnect()
-
 	page := browser.MustPage()
+
+	defer page.Close()
 
 	page.SetUserAgent(&proto.NetworkSetUserAgentOverride{
 		UserAgent: "cedisearchbot/0.1 (+https://cedi-search.vercel.app/about)",
