3ff2a30df53bbc1bfdcbca0123e12860ad99b10a
owbird <owbirdphorccuo@gmail.com>
2023-11-17 04:40:59 +0000

    refactor

--- main.go
+++ main.go
@@ -11,15 +11,12 @@ import (
 	"github.com/Cedi-Search/Cedi-Search-Engine/ishtari"
 	"github.com/Cedi-Search/Cedi-Search-Engine/jiji"
 	"github.com/Cedi-Search/Cedi-Search-Engine/jumia"
-	"github.com/anaskhan96/soup"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 
 	log.Println("[+] Startup")
-
-	soup.Header("User-Agent", "cedisearchbot/0.1 (+https://cedi-search.vercel.app/about)")
 
 	wg := sync.WaitGroup{}
 
