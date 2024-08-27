7c39a928153f53c68af4fefdc0cdec31bba09fb3
owbird <owbirdphorccuo@gmail.com>
2024-08-27 14:12:43 +0000

    chore (utils): Support multiple slice types for shuffling

--- utils/shuffleLinks.go
+++ utils/shuffleLinks.go
@@ -7,10 +7,13 @@ import (
 	"github.com/anaskhan96/soup"
 )
 
-// ShuffleLinks shuffles the order of links.
-func ShuffleLinks(links []soup.Root) {
-	rand.New(rand.NewSource(time.Now().UnixNano()))
+type Link interface {
+	soup.Root | string
+}
 
+// ShuffleLinks shuffles the order of links.
+func ShuffleLinks[T Link](links []T) {
+	rand.New(rand.NewSource(time.Now().UnixNano()))
 	rand.Shuffle(len(links), func(i, j int) {
 		links[i], links[j] = links[j], links[i]
 	})
