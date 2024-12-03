090791cad62d080ddf97ee9f5b0ba7dc8fbb9d80
owbird <owbirdphorccuo@gmail.com>
2024-12-03 11:55:38 +0000

    refactor (utils): Refactor logger

--- utils/logger.go
+++ utils/logger.go
@@ -1,7 +1,6 @@
 package utils
 
 import (
-	"fmt"
 	"log"
 	"path/filepath"
 	"time"
@@ -20,92 +19,68 @@ const (
 	Error    LogType = "error"
 	Utils    LogType = "utils"
 	Default  LogType = "default"
+
+	LogFormat = "[{{datetime}}] [{{level}}] [{{scope}}] {{message}} {{data}} {{extra}}\n"
 )
 
 func Logger(logType LogType, scope string, stmts ...interface{}) {
 	defer slog.Close()
 
 	today := time.Now().Format("2006-01-02")
+	logDir := filepath.Join("logs", today)
 
-	fmtLogDir := func(scope string) string {
-		logDir := filepath.Join("logs", today, fmt.Sprintf("%s.log", scope))
+	createFileHandler := func(fileName string) slog.Handler {
+		logPath := filepath.Join(logDir, fileName)
+		h, err := handler.NewFileHandler(logPath)
+		if err != nil {
+			log.Fatalf("Failed to create log file %s: %v", logPath, err)
+		}
 
-		return logDir
+		fileFormatter := slog.NewTextFormatter()
+		fileFormatter.SetTemplate(LogFormat)
+
+		h.SetFormatter(fileFormatter)
+		return h
 	}
 
-	l := slog.New()
+	getLogLevel := func() slog.Level {
+		switch logType {
+		case Error:
+			return slog.ErrorLevel
 
-	formatter := slog.NewTextFormatter()
-	formatter.SetTemplate("[{{datetime}}] [{{level}}] [{{scope}}] {{message}} {{data}} {{extra}}\n")
-	formatter.EnableColor = true
+		case Default:
+			return slog.DebugLevel
+
+		default:
+			return slog.InfoLevel
+		}
+
+	}
+
+	consoleFormatter := slog.NewTextFormatter()
+	consoleFormatter.SetTemplate(LogFormat)
+	consoleFormatter.EnableColor = true
 
 	consoleHandler := handler.NewConsoleHandler(slog.AllLevels)
-	consoleHandler.SetFormatter(formatter)
+	consoleHandler.SetFormatter(consoleFormatter)
 
+	l := slog.New()
 	l.AddHandlers(consoleHandler)
 
-	crawlerFileHandler, err := handler.NewFileHandler(fmtLogDir("crawler"), handler.WithLogfile("crawler.log"))
-	crawlerFileHandler.SetFormatter(formatter)
-	if err != nil {
-		log.Fatalf("Failed to create crawler log file: %v", err)
+	handlers := map[LogType]slog.Handler{
+		Crawler:  createFileHandler("crawler.log"),
+		Indexer:  createFileHandler("indexer.log"),
+		Sniffer:  createFileHandler("sniffer.log"),
+		Error:    createFileHandler("error.log"),
+		Database: createFileHandler("database.log"),
+		Utils:    createFileHandler("utils.log"),
 	}
 
-	indexerFileHandler, err := handler.NewFileHandler(fmtLogDir("indexer"), handler.WithLogfile("indexer.log"))
-	indexerFileHandler.SetFormatter(formatter)
-	if err != nil {
-		log.Fatalf("Failed to create indexer log file: %v", err)
+	if h, ok := handlers[logType]; ok {
+		l.AddHandlers(h)
 	}
 
-	snifferFileHandler, err := handler.NewFileHandler(fmtLogDir("sniffer"), handler.WithLogfile("sniffer.log"))
-	snifferFileHandler.SetFormatter(formatter)
-	if err != nil {
-		log.Fatalf("Failed to create sniffer log file: %v", err)
-	}
-
-	errorFileHandler, err := handler.NewFileHandler(fmtLogDir("error"), handler.WithLogfile("error.log"))
-	errorFileHandler.SetFormatter(formatter)
-	if err != nil {
-		log.Fatalf("Failed to create error log file: %v", err)
-	}
-
-	databaseFileHandler, err := handler.NewFileHandler(fmtLogDir("database"), handler.WithLogfile("database.log"))
-	databaseFileHandler.SetFormatter(formatter)
-	if err != nil {
-		log.Fatalf("Failed to create database log file: %v", err)
-	}
-
-	utilsFileHandler, err := handler.NewFileHandler(fmtLogDir("utils"), handler.WithLogfile("database.log"))
-	utilsFileHandler.SetFormatter(formatter)
-	if err != nil {
-		log.Fatalf("Failed to create database log file: %v", err)
-	}
-
-	switch logType {
-	case Crawler:
-		l.AddHandlers(crawlerFileHandler)
-	case Indexer:
-		l.AddHandlers(indexerFileHandler)
-	case Sniffer:
-		l.AddHandlers(snifferFileHandler)
-	case Error:
-		l.AddHandlers(errorFileHandler)
-	case Database:
-		l.AddHandlers(databaseFileHandler)
-	case Utils:
-		l.AddHandlers(utilsFileHandler)
-	}
-
-	if logType == Error {
-		l.WithFields(slog.M{
-			"scope": scope,
-		}).Error(stmts...)
-	} else if logType == Default {
-		l.WithFields(slog.M{
-			"scope": scope,
-		}).Debug(stmts...)
-	} else {
-		l.WithFields(slog.M{
-			"scope": scope,
-		}).Info(stmts...)
-	}
+	l.WithFields(slog.M{
+		"scope": scope,
+	}).Log(getLogLevel(), stmts...)
 }
